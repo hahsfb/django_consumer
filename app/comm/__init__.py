034527f9dddf9ceec422c40cae6f0099b77e78eb
hqj <hao13771073133>
2018-11-16 17:30:32 +0800

    add comm

---
+++ app/comm/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File    : __init__.py.py
+@Author  : HaoQiangJian
+@Site    : 
+@Time    : 18-10-22 下午4:48
+@Version : 
+"""
---
+++ app/comm/rabbit_mq.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File    : rabbit_mq.py
+@Author  : HaoQiangJian
+@Site    : 
+@Time    : 18-11-13 下午5:03
+@Version : 
+"""
+
+import pika, json
+from .config import MQ_HOST, MQ_PORT, MQ_USR, MQ_PWD
+
+
+class MessageMQ:
+    def __init__(self, queue='hello'):
+        self.conn = pika.BlockingConnection(
+            pika.ConnectionParameters(host=MQ_HOST, port=MQ_PORT, virtual_host='/',
+                                      credentials=pika.PlainCredentials(MQ_USR, MQ_PWD))
+        )
+        self.channel = self.conn.channel()
+        self.channel.exchange_declare(exchange='order-exchange', durable=True, exchange_type='topic')
+        self.channel.queue_declare(queue=queue, durable=True)
+
+    def basic_publish(self, routing_key, body, exchange='order-exchange'):
+        return self.channel.basic_publish(
+            exchange=exchange,
+            routing_key=routing_key,
+            body=body,
+            properties=pika.BasicProperties(
+                delivery_mode=2,
+            )
+        )
+
+    def close(self):
+        return self.conn.close()
+
+
+if __name__ == '__main__':
+    mq = MessageMQ(queue='order-queue')
+    aa = mq.basic_publish('order.def', json.dumps({'x': 2, 'y': 6}))
+    mq.conn.close()
+    print(aa)
+    print()
+
+
+
+"""
+credentials = pika.PlainCredentials('', '123456')
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+    '192.168.56.19', 5672, '/', credentials))
+channel = connection.channel()
+
+# 声明queue
+channel.queue_declare(queue='balance')
+
+# n RabbitMQ a message can never be sent directly to the queue, it always needs to go through an exchange.
+channel.basic_publish(exchange='',
+                      routing_key='balance',
+                      body='Hello World!')
+print(" [x] Sent 'Hello World!'")
+connection.close()
+"""
