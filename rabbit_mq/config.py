9f61e9c7764318b91479ba7c57d89ca1a4cf763d
hqj <hao13771073133>
2018-12-07 20:05:15 +0800

    Update model

---
+++ rabbit_mq/config.py
@@ -0,0 +1,27 @@
+"""
+缓存信息配置
+"""
+REDIS_HOST = '127.0.0.1'
+REDIS_PORT = 6379
+REDIS_DB = 0
+REDIS_PWD = ''
+
+"""
+数据库的配置
+"""
+DB_HOST = '127.0.0.1'
+DB_PORT = 3306
+DB_USER = 'root'
+DB_PWD = ''
+DB_NAME = 'test'
+
+"""
+RabbitMQ
+"""
+MQ_HOST = '127.0.0.1'
+MQ_PORT = 5672
+MQ_USR = 'guest'
+MQ_PWD = 'guest'
+MQ_ORDER_TOPIC = 'admin_order'
+CRON_TIMES = 3
+
